d98f19ffcce2d845636db666626adf044d1c7968
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-11-24 17:40:13 +0100

    new test

---
+++ tests/test_formatter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test correct string formatting.
+"""
+from __future__ import division, print_function
+import sys
+from os.path import abspath, dirname
+
+import numpy as np
+
+# Add parent directory to beginning of path variable
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+from shapeout.util import float2string_nsf
+
+
+def test_string_formatter():
+    a = 1.23456789
+    b = a*10.**(np.arange(-10, 10))
+    n=4
+    
+    results = []
+    
+    for fval in b:
+        results.append(float2string_nsf(fval, n=n))
+
+    shouldbe = ["0.00000000012346",
+                "0.0000000012346",
+                "0.000000012346",
+                "0.00000012346",
+                "0.0000012346",
+                "0.000012346",
+                "0.00012346",
+                "0.0012346",
+                "0.012346",
+                "0.12346",
+                "1.2346",
+                "12.346",
+                "123.46",
+                "1234.6",
+                "12346",
+                "123457",
+                "1234568",
+                "12345679",
+                "123456789",
+                "1234567890",]
+
+    for ii in range(len(b)):
+        assert shouldbe[ii] == results[ii]
+
+
+if __name__ == "__main__":
+    # Run all tests
+    loc = locals()
+    for key in list(loc.keys()):
+        if key.startswith("test_") and hasattr(loc[key], "__call__"):
+            loc[key]()
+
+        
