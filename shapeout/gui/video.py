4bf67a1185071cac1865b0cf75f05560fa091014
paulmueller <paul.mueller@biotec.tu-dresden.de>
2016-05-23 00:11:44 +0200

    hide/show fluorescence panel

--- shapeout/gui/video.py
+++ shapeout/gui/video.py
@@ -31,6 +31,7 @@ class ImagePanel(ScrolledPanel):
     def __init__(self, parent, frame):
         ScrolledPanel.__init__(self, parent, -1)
         self.frame = frame
+        self.parent = parent
 
         self.SetupScrolling(scroll_y=True, scroll_x=True)
 
@@ -82,7 +83,7 @@ class ImagePanel(ScrolledPanel):
         #self.plot_window.redraw()
 
         # Draw image with wxPython instead
-        self.startSizeX = 230
+        self.startSizeX = 250
         self.startSizeY = 77
         self.img = wx.EmptyImage(self.startSizeX, self.startSizeY)
         self.imageCtrl = wx.StaticBitmap(self, wx.ID_ANY, 
@@ -138,14 +139,18 @@ class ImagePanel(ScrolledPanel):
         
         self.plot_window = Window(self, component=container)
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(ctrlsizer, 0)
-        sizer.Add(self.imageCtrl, 2, wx.EXPAND, 0)
-        sizer.Add(exclsizer, 0, wx.EXPAND)
-        sizer.Add(self.plot_window.control, 3, wx.EXPAND, 0)
-        self.SetSizer(sizer)
+        sizer = wx.GridBagSizer(5,5)
+        sizer.Add(ctrlsizer, (0,0))
+        sizer.Add(self.imageCtrl, (1,0))
+        sizer.Add(exclsizer, (2,0))
+        self.plot_window.control.SetMinSize((300, 300))
+        sizer.Add(self.plot_window.control, (3,0), span=(2,2), flag=wx.EXPAND)
 
-    
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        self.sizer = sizer
+
+
     def OnChBoxExclude(self, e=None):
         """ If the exclude-check box is triggered, change the
         corresponding value in the measurement."""
@@ -176,76 +181,80 @@ class ImagePanel(ScrolledPanel):
 
 
     def ShowEvent(self, mm_id, evt_id):
-            """
-            Parameters
-            ----------
+        """
+        Parameters
+        ----------
+        
+        mm_id : int
+            measurement identifier (index in self.analysis.measurements)
+        evt_id : int
+            frame identifier, starts at 0
+        
+        """
+        mm = self.analysis.measurements[mm_id]
+        # Taking the abspath of the video does not always work with OpenCV?
+        #vfile = os.path.join(dataset.fdir, dataset.video)
+        old_dir = os.getcwd()
+        os.chdir(mm.fdir)
+        video = cv2.VideoCapture(mm.video)
+        os.chdir(old_dir)
+        totframes = video.get(cv_const.CV_CAP_PROP_FRAME_COUNT)
+        
+        # determine video file offset. Some RTDC setups
+        # do not record the first image of a video.
+        frames_skipped = mm.Configuration["General"]["Video Frame Offset"]
+        actual_sel_offset = evt_id - frames_skipped
+        if actual_sel_offset < 0:
+            # Display an empty image if there is no image for the event
+            warnings.warn("No image for event {}.".format(evt_id))
+            self.PlotImage(None)
+            self.UpdateSelections()
+        else:
+            video.set(cv_const.CV_CAP_PROP_POS_FRAMES, actual_sel_offset)
             
-            mm_id : int
-                measurement identifier (index in self.analysis.measurements)
-            evt_id : int
-                frame identifier, starts at 0
-            
-            """
-            mm = self.analysis.measurements[mm_id]
-            # Taking the abspath of the video does not always work with OpenCV?
-            #vfile = os.path.join(dataset.fdir, dataset.video)
-            old_dir = os.getcwd()
-            os.chdir(mm.fdir)
-            video = cv2.VideoCapture(mm.video)
-            os.chdir(old_dir)
-            totframes = video.get(cv_const.CV_CAP_PROP_FRAME_COUNT)
-            
-            # determine video file offset. Some RTDC setups
-            # do not record the first image of a video.
-            frames_skipped = mm.Configuration["General"]["Video Frame Offset"]
-            actual_sel_offset = evt_id - frames_skipped
-            if actual_sel_offset < 0:
-                # Display an empty image if there is no image for the event
-                warnings.warn("No image for event {}.".format(evt_id))
-                self.PlotImage(None)
-                self.UpdateSelections()
-            else:
-                video.set(cv_const.CV_CAP_PROP_POS_FRAMES, actual_sel_offset)
+            flag, cellimg = video.read()
+
+            if flag:
+                # add contour in red
+                if len(cellimg.shape) == 2:
+                    # convert grayscale to color
+                    cellimg = np.tile(cellimg, [3,1,1]).transpose(1,2,0)
                 
-                flag, cellimg = video.read()
-    
-                if flag:
-                    # add contour in red
-                    if len(cellimg.shape) == 2:
-                        # convert grayscale to color
-                        cellimg = np.tile(cellimg, [3,1,1]).transpose(1,2,0)
+                
+                r = cellimg[:,:,0]
+                b = cellimg[:,:,1]
+                g = cellimg[:,:,2]
+                
+                # only do this if there was a contour file loaded
+                if len(mm.contours) > 0:
+                    contours = mm.contours[mm.frame[evt_id]]
                     
-                    
-                    r = cellimg[:,:,0]
-                    b = cellimg[:,:,1]
-                    g = cellimg[:,:,2]
-                    
-                    # only do this if there was a contour file loaded
-                    if len(mm.contours) > 0:
-                        contours = mm.contours[mm.frame[evt_id]]
-                        
-                        r[contours[:,1], contours[:,0]] = 255
-                        b[contours[:,1], contours[:,0]] = 0
-                        g[contours[:,1], contours[:,0]] = 0
-                    
-                    self.frame.ImageArea.PlotImage(cellimg)
-                    
-                    # measurement id
-                    mm_id = self.analysis.measurements.index(mm)
-                    self.UpdateSelections(mm_id=mm_id, evt_id=evt_id)
-                    
-            
-            video.release()
-            print("Frame {} / {}".format(evt_id, totframes))
+                    r[contours[:,1], contours[:,0]] = 255
+                    b[contours[:,1], contours[:,0]] = 0
+                    g[contours[:,1], contours[:,0]] = 0
+                
+                self.frame.ImageArea.PlotImage(cellimg)
+                
+                # measurement id
+                mm_id = self.analysis.measurements.index(mm)
+                self.UpdateSelections(mm_id=mm_id, evt_id=evt_id)
+                
+        
+        video.release()
+        print("Frame {} / {}".format(evt_id, totframes))
 
-            # Update exclude check-box
-            self.WXChB_exclude.SetValue(not mm._filter_manual[evt_id])
+        # Update exclude check-box
+        self.WXChB_exclude.SetValue(not mm._filter_manual[evt_id])
 
-            # Set max value for spin control
-            max_evt = self.analysis.measurements[mm_id].time.shape[0]
-            self.WXSP_plot.SetRange(1, max_evt)
+        # Set max value for spin control
+        max_evt = self.analysis.measurements[mm_id].time.shape[0]
+        self.WXSP_plot.SetRange(1, max_evt)
 
-            # Plot traces
+        # Plot traces
+        if len(list(mm.traces)) != 0:
+
+            self.plot_window.control.Show(True)
+
             for key in list(mm.traces.keys()):
                 data = mm.traces[key][evt_id]
                 self.trace_data.set_data(key, data)
@@ -258,6 +267,12 @@ class ImagePanel(ScrolledPanel):
                         okey == "x"):
                     self.trace_data.set_data(okey, zerodata)
 
+        else:
+            self.plot_window.control.Show(False)
+           
+        self.Layout()
+        self.GetParent().Layout()
+       
 
     def PlotImage(self, image=None):
         def pil_to_wx_bmp(image):
@@ -291,7 +306,7 @@ class ImagePanel(ScrolledPanel):
         self.img = wx.BitmapFromImage(wximg)
         self.imageCtrl.SetBitmap(self.img)
         # Redraw the panel to prevent artifact images on Windows
-        self.Layout()
+        #self.Layout()
 
 
     def UpdateAnalysis(self, analysis):
