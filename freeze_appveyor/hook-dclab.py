d1f05a1394aea511be6fd3e51e6f2766e1cc23cd
paulmueller <paulmueller@users.noreply.github.com>
2017-02-17 20:55:13 +0100

    fix hook for dclab

--- freeze_appveyor/hook-dclab.py
+++ freeze_appveyor/hook-dclab.py
@@ -2,9 +2,11 @@ from pkg_resources import resource_filename  # @UnresolvedImport
 
 import dclab
 cfgfile = resource_filename("dclab", 'dclab.cfg')
+cfgfile2 = resource_filename("dclab.rtdc_dataset", 'config_default.cfg')
 
 ## dclab files
 datas = [(cfgfile, "dclab")]
+datas += [(cfgfile2, "dclab/rtdc_dataset")]
 
 hiddenimports = ["nptdms", "nptdms.version", "nptdms.tdms", "nptdms.tdmsinfo"]
 hiddenimports += ["scipy.stats"]
