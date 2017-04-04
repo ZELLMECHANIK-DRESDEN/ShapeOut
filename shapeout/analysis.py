fix accuracy computation when emodulus is computed and automatically compute emodulus if axis is selected

shapeout/analysis.py
@@ -32,7 +32,7 @@ class Analysis(object):
      - common configuration parameters of the data sets
      - Plotting parameters
     """
-    def __init__(self, data, search_path="./"):
+    def __init__(self, data, search_path="./", config={}):
         """ Analysis data object.
         """
         self.measurements = []
@@ -51,9 +51,15 @@ class Analysis(object):
         else:
             raise ValueError("Argument not an index file or list of"+\
                              " .tdms files: {}".format(data))
+        # Set configuration (e.g. from previous analysis)
+        if len(config):
+            self.SetParameters(config)
         # Complete missing configuration parameters
         self._complete_config()
-
+        # Complete missing data columns
+        self._complete_data()
+        # Reset contour accuracies
+        self.init_plot_accuracies()
 
     def _complete_config(self):
         """Complete configuration of all RTDC_DataSet sets
@@ -87,6 +93,13 @@ class Analysis(object):
                 for a in appends:
                     if not item+a in mm.config["plotting"]:
                         mm.config["plotting"][item+a] = 0
+
+
+    def _complete_data(self):
+        """Computes missing data columns if necessary"""
+        axes = self.GetPlotAxes()
+        if "emodulus" in axes:
+            self.compute_emodulus()
 
 
     def _clear(self):
@@ -234,10 +247,6 @@ class Analysis(object):
                     px_um=mm.config["image"]["pix size"],
                     temperature=mm.config["calculation"]["emodulus temperature"])
             mm.emodulus=emod
-            
-            for key in ["kde accuracy emodulus", "contour accuracy emodulus"]:
-                if key in mm.config["plotting"]: 
-                    mm.config["plotting"].pop(key)
         
         self._complete_config()
 
@@ -516,6 +525,38 @@ class Analysis(object):
         return conf
 
 
+    def init_plot_accuracies(self):
+        """ Set initial (heuristic) accuracies for all plots.
+        
+        It is not always easy to determine the correct accuracy for
+        the contour plots. This method sets these accuracies for the user.
+        
+        All keys of the active axes are changed, e.g.:
+          - "contour accuracy area"
+          - "contour accuracy defo"
+          - "kde accuracy defo"
+          - "kde accuracy defo"
+        
+        Note that the accuracies are not updated when the key
+        ["Plotting"]["Contour Fix Scale"] is set to `True` for the
+        first measurement of the analysis.
+        """
+        # check if updating is disabled:
+        if self.measurements[0].config["plotting"]["contour fix scale"]:
+            return
+        
+        # Remove contour accuracies for the current plots
+        for key in dfn.uid:
+            for mm in self.measurements:
+                for var in ["contour accuracy {}".format(key),
+                            "kde accuracy {}".format(key)]:
+                    if var in mm.config["plotting"]:
+                        mm.config["plotting"].pop(var)
+
+        # Set default accuracies
+        self._complete_config()
+
+
     def PolygonFilterRemove(self, filt):
         """
         Removes a polygon filter from all elements of the analysis.
@@ -525,54 +566,6 @@ class Analysis(object):
                 mm.PolygonFilterRemove(filt)
             except ValueError:
                 pass
-
-
-    def SetContourAccuracies(self, points=70):
-        """ Set initial (heuristic) accuracies for all plots.
-        
-        It is not always easy to determine the correct accuracy for
-        the contour plots. This method sets these accuracies for the
-        active axes for the user. Each axis is divided into `points`
-        segments and the length of each segment is then used for the
-        accuracy.
-        
-        All keys of the active axes are changed, e.g.:
-          - "Contour Accuracy Area"
-          - "Contour Accuracy Defo"
-        
-        Note that the accuracies are not updated when the key
-        ["Plotting"]["Contour Fix Scale"] is set to `True` for the
-        first measurement of the analysis.
-        """
-        # check if updating is disabled:
-        if self.measurements[0].config["Plotting"]["Contour Fix Scale"]:
-            return
-        
-        if len(self.measurements) > 1:
-            # first create dictionary with min/max keys
-            minmaxdict = dict()
-            for name in dfn.uid:
-                minmaxdict["{} Min".format(name)] = list()
-                minmaxdict["{} Max".format(name)] = list()
-                
-            for mm in self.measurements:
-                # uid is defined in definitions
-                for name in dfn.uid:
-                    if hasattr(mm, dfn.cfgmaprev[name]):
-                        att = getattr(mm, dfn.cfgmaprev[name])
-                        minmaxdict["{} Min".format(name)].append(att.min())
-                        minmaxdict["{} Max".format(name)].append(att.max())
-            # set contour accuracy for every element
-            for name in dfn.uid:
-                atmax = np.average(minmaxdict["{} Max".format(name)])
-                atmin = np.average(minmaxdict["{} Min".format(name)])
-                acc = (atmax-atmin)/points
-                # round to 2 significant digits
-                acg = float("{:.1e}".format(acc))
-                acm = float("{:.1e}".format(acc*2))
-                for mm in self.measurements:
-                    mm.config["Plotting"]["Contour Accuracy {}".format(name)] = acg
-                    mm.config["Plotting"]["KDE Accuracy {}".format(name)] = acm
 
 
     def SetContourColors(self, colors=None):
@@ -594,8 +587,8 @@ class Analysis(object):
                     newcolors.append(color)
                 colors = newcolors
 
-            for i, mm in enumerate(self.measurements):
-                mm.config["Plotting"]["Contour Color"] = colors[i]
+            for ii, mm in enumerate(self.measurements):
+                mm.config["plotting"]["contour color"] = colors[ii]
 
 
     def SetParameters(self, newcfg):