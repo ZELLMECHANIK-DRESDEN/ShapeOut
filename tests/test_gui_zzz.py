8add222a7345b8aa269ea61900280427485b633b
paulmueller <paulmueller@users.noreply.github.com>
2017-07-03 12:32:04 +0200

    try to repair tests

---
+++ tests/test_gui_zzz.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This must be the last test executed for some unknown reason"""
+from __future__ import division, print_function
+import sys
+from os.path import abspath, dirname
+
+import numpy as np
+import tempfile
+
+import dclab
+
+# Add parent directory to beginning of path variable
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import shapeout
+
+
+def test_final():
+    from shapeout.__main__ import prepare_app
+    prepare_app()
+
+
+if __name__ == "__main__":
+    # Run all tests
+    loc = locals()
+    for key in list(loc.keys()):
+        if key.startswith("test_") and hasattr(loc[key], "__call__"):
+            loc[key]()
