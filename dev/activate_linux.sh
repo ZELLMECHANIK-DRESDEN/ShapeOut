d144d16976c1d55c2509c92af51d86caa67fe64f
paulmueller <paulmueller@users.noreply.github.com>
2017-09-11 21:14:48 +0200

    update dev environment script

--- dev/activate_linux.sh
+++ dev/activate_linux.sh
@@ -4,13 +4,16 @@
 DIRECTORY=".env_shapeout"
 
 echo "------------------------------------------------------------------"
-echo "Make sure you dclab checked out in the parent directory:"
+echo "Make sure you dclab and fcswrite are checked out in the parent directory:"
 echo "   https://github.com/ZELLMECHANIK-DRESDEN/dclab.git"
+echo "   https://github.com/ZELLMECHANIK-DRESDEN/fcswrite.git"
 echo "------------------------------------------------------------------"
 echo "Make sure you have the following packages installed:"
-echo "   python-numpy python-opencv python-wxgtk2.8 r-base r-recommended"
+echo "   python-chaco python-numpy python-scipy python-wxgtk2.8 r-base r-recommended"
 echo "------------------------------------------------------------------"
 
+## old dir ##
+_old="$(pwd)"
 ## who am i? ##
 _script="$(readlink -f ${BASH_SOURCE[0]})"
 ## Delete last component from $_script ##
@@ -20,16 +23,18 @@ cd $_base
 if [ ! -d $DIRECTORY ]; then
     virtualenv --system-site-packages $DIRECTORY
     source "$DIRECTORY/bin/activate"
-    #pip install pyinstaller==2.0
     pip install git+git://github.com/pyinstaller/pyinstaller.git@779d07b236a943a4bf9d2b1a0ae3e0ebcc914798
     pip install nptdms
     pip install statsmodels==0.6.1
     pip install kiwisolver
     pip install pyper
-    #deactivate
-    
-    # register dclab
+    pip install imageio
+    pip install appdirs
+    pip install simplejson
+
+    # register dclab and fcswrite
     ln -s "../../../../../../dclab/dclab" "$DIRECTORY/lib/python2.7/site-packages"
+    ln -s "../../../../../../fcswrite/fcswrite" "$DIRECTORY/lib/python2.7/site-packages"
 
 	# install lme4 package
 	R -e "install.packages('lme4', repos='http://cran.r-project.org')"
@@ -37,4 +42,5 @@ fi
 
 source "${_base}/${DIRECTORY}/bin/activate"
 
-cd ..
+cd $_old
+
